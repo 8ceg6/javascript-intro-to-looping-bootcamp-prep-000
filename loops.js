a62c91e5c09767e64264e0bf5a1606d2d2692f4a
cedric e goddard <8ceg6@comcast.net>
2019-07-15 19:05:37 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+
+
+function forLoop(array){
+          for (let i= 0; i< 25; i++){ 
+              if (i===1) {
+          
+                array.push('I am 1 strange loop.')}
+                 
+                else {array.push('I am 24 strange loops.')} 
+  
+}
+  return array;
+  }
+  function whileLoop(array){
+  
+          while (array > 0) {
+          console.log (--array);
+    }
+          return 'done';
+  }
+  var i=0;
+  
+ function doWhileLoop(){             
+  i= i+1;
+  return i;
+ }
+   do {console.log("I run once regardless");
+   }
+   
+             while (incrementVariabe() < 0)             }    
+ }
